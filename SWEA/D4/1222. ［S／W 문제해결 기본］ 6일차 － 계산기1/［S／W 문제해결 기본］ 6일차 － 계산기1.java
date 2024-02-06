cef90313260aea885a6eb4fecdc04bbfdc8d028e
JinHyunji <131237930+JinHyunji@users.noreply.github.com>
2024-02-06 14:33:29 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기1, Time: 118 ms, Memory: 18,664 KB -BaekjoonHub

---
+++ SWEA/D4/1222. ［S／W 문제해결 기본］ 6일차 － 계산기1/［S／W 문제해결 기본］ 6일차 － 계산기1.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Solution {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String postfix;
+
+		Stack<Character> op;
+		Stack<Integer> od;
+
+		int answer;
+
+		for (int t = 1; t <= 10; t++) {
+			int len = Integer.parseInt(br.readLine());
+			String expression = br.readLine();
+			char c;
+
+			// 중위 표기식 -> 후위 표기식
+			postfix = "";
+			op = new Stack<>();
+
+			for (int i = 0; i < len; i++) {
+				c = expression.charAt(i);
+				
+				if ('0' <= c && c <= '9')
+					postfix += c;
+				else {
+					if (op.isEmpty())
+						op.push(c);
+					else {
+						postfix += op.pop();
+						op.push(c);
+					}
+				}
+			}
+			
+			while (!op.isEmpty()) {
+				postfix += op.pop();
+			}
+
+			// 후위 표기식 -> 계산
+			od = new Stack<>();
+			int result;
+			
+			for (int i = 0; i < postfix.length(); i++) {
+				c = postfix.charAt(i);
+
+				if ('0' <= c && c <= '9')
+					od.push(c - '0');
+				else {
+					int B = od.pop();
+					int A = od.pop();
+					
+					result = A + B;
+					od.push(result);
+				}
+			}
+			
+			answer = od.pop();
+			System.out.println("#" + t + " " + answer);
+		}
+	}
+}
