fd72a339b565aa73f44887b4e2e163ded0a9a7dd
JinHyunji <131237930+JinHyunji@users.noreply.github.com>
2024-02-15 17:43:55 +0900

    [D3] Title: 농작물 수확하기, Time: 117 ms, Memory: 20,600 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,12 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Solution {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 
 		int answer;
 		
