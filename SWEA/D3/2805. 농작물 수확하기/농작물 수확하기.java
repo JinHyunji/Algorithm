ce28e0990568ab44e9149934670e3f48cc1c70cb
JinHyunji <131237930+JinHyunji@users.noreply.github.com>
2024-02-15 17:42:50 +0900

    [D3] Title: 농작물 수확하기, Time: 117 ms, Memory: 20,600 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -8,7 +8,6 @@ public class Solution {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
-		int[][] map;
 		int answer;
 		
 		int T = Integer.parseInt(br.readLine()); // 테스트 케이스 입력
@@ -18,21 +17,11 @@ public class Solution {
 			int N = Integer.parseInt(br.readLine());
 			answer = 0;
 			
-			// 농장 배열 생성
-			map = new int[N][N];
-
-			// 농장 수익 초기화
 			for (int r = 0; r < N; r++) {
 				String input = br.readLine();
-				for (int c = 0; c < N; c++) {
-					map[r][c] = input.charAt(c) - '0';
-				}
-			}
-			
-			for (int r = 0; r < N; r++) {
 				int start = Math.abs(N/2 - r);
 				for (int c = start; c < N - start; c++) {
-					answer += map[r][c];
+					answer += (input.charAt(c) - '0');
 				}
 			}
 			
