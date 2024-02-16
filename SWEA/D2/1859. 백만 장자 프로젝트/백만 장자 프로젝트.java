cfc1e2b357f5cab1ff01cc6186172d3bfa23dcb3
JinHyunji <131237930+JinHyunji@users.noreply.github.com>
2024-02-16 17:19:29 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 498 ms, Memory: 134,988 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
@@ -2,37 +2,34 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-
-		int T = Integer.parseInt(br.readLine());
-		int[] arr;
-		int[] maxList;
-		Long profit;
-
-		for (int t = 1; t <= T; t++) {
-			int N = Integer.parseInt(br.readLine());
-			arr = new int[N];
-			maxList = new int[N];
-
-			profit = 0L;
-			int max = 0;
-			st = new StringTokenizer(br.readLine());
-			for (int n = 0; n < N; n++) {
-				arr[n] = Integer.parseInt(st.nextToken());
-			}
-
-			for (int n = N - 1; n >= 0; n--) {
-				if (arr[n] > max)
-					max = arr[n];
-				maxList[n] = max;
-				profit += (maxList[n] - arr[n]);
-			}
-			
-			System.out.println("#" + t + " " + profit);
-		}
-	}
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        int T = Integer.parseInt(br.readLine());
+        int[] arr;
+        Long profit;
+ 
+        for (int t = 1; t <= T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            arr = new int[N];
+ 
+            profit = 0L;
+            int max = 0;
+            st = new StringTokenizer(br.readLine());
+            for (int n = 0; n < N; n++) {
+                arr[n] = Integer.parseInt(st.nextToken());
+            }
+ 
+            for (int n = N - 1; n >= 0; n--) {
+                if (arr[n] > max)
+                    max = arr[n];
+                profit += (max - arr[n]);
+            }
+             
+            System.out.println("#" + t + " " + profit);
+        }
+    }
 }
