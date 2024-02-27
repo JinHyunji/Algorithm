3b0d472522239e2b786e91b5162b471a44daca64
JinHyunji <131237930+JinHyunji@users.noreply.github.com>
2024-02-27 16:21:36 +0900

    [Silver II] Title: DFS와 BFS, Time: 364 ms, Memory: 21556 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -7,68 +7,72 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+	static boolean[][] data;
+	static boolean[] visited;
 	static int N, M, V;
-	static int[][] adjMatrix;
-	static int[] visited;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
+		// 정점개수, 간선개수, 탐색시작정점번호 순으로 입력
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		V = Integer.parseInt(st.nextToken());
 
-		adjMatrix = new int[N + 1][N + 1];
-		visited = new int[N + 1];
+		data = new boolean[N + 1][N + 1];
+		visited = new boolean[N + 1];
 
-		for (int m = 0; m < M; m++) {
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-
-			adjMatrix[a][b] = 1;
-			adjMatrix[b][a] = 1;
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			data[n][m] = true;
+			data[m][n] = true;
 		}
 
-		visited[V] = 1;
+		// DFS
+		visited[V] = true;
 		DFS(V);
 
-		visited = new int[N + 1];
+		// BFS
 		System.out.println();
+		visited = new boolean[N + 1];
 		BFS();
 	}
 
+	// 깊이 우선 탐색
 	public static void DFS(int node) {
+		// base case
 		System.out.print(node + " ");
 
+		// recursive case
 		for (int next = 1; next <= N; next++) {
-			if (visited[next] == 1 || adjMatrix[node][next] == 0)
-				continue;
+			if (visited[next] == false && data[node][next] == true && data[next][node] == true) {
+				visited[next] = true;
+				DFS(next);
+			}
 
-			visited[next] = 1;
-			DFS(next);
 		}
+
 	}
 
+	// 너비 우선 탐색
 	public static void BFS() {
 		Queue<Integer> q = new LinkedList<>();
-		
-		visited[V] = 1;
 		q.offer(V);
+		visited[V] = true;
 
 		while (!q.isEmpty()) {
 			int node = q.poll();
 			System.out.print(node + " ");
 			
 			for (int next = 1; next <= N; next++) {
-				if (visited[next] == 1 || adjMatrix[node][next] == 0)
-					continue;
-				visited[next] = 1;
-				q.offer(next);
+				if (data[node][next] == true && data[next][node] == true && visited[next] == false) {
+					q.offer(next);
+					visited[next] = true;
+				}
 			}
 		}
 	}
-
 }
