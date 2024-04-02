d0ecd304eeb1208509dff781916d50435aafd517
Hyun-ji <131237930+JinHyunji@users.noreply.github.com>
2024-04-02 11:57:56 +0900

    [Bronze IV] Title: 특별한 가지, Time: 204 ms, Memory: 17664 KB -BaekjoonHub

--- 백준/Bronze/31668. 특별한 가지/특별한 가지.java
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.java
@@ -1,9 +1,2 @@
-import java.util.Scanner;
-public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt(); int M = sc.nextInt(); int K = sc.nextInt();
-		int result = M / N * K;
-		System.out.println(result);
-	}
-}
+import java.util.*;
+public class Main {public static void main(String[] args) {Scanner sc=new Scanner(System.in);int N=sc.nextInt();System.out.print(sc.nextInt()/N*sc.nextInt());}}
