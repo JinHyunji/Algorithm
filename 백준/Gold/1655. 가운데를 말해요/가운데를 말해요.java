7b2658ffbda7e344d685ad03474fc832ffc9a83b
Hyun-ji <131237930+JinHyunji@users.noreply.github.com>
2024-04-13 23:51:07 +0900

    Update 가운데를 말해요.java

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -8,29 +8,24 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
+
 		int N = Integer.parseInt(br.readLine());
 		PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Comparator.reverseOrder());
 		PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
 
 		for (int i = 0; i < N; i++) {
 			int a = Integer.parseInt(br.readLine());
-			
-			if (maxHeap.size() == minHeap.size()) {
+
+			if (maxHeap.size() == minHeap.size())
 				maxHeap.add(a);
-				
-				if (!minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()) {
-					minHeap.add(maxHeap.poll());
-					maxHeap.add(minHeap.poll());
-				}
-			} else {
+			else
 				minHeap.add(a);
-				
-				if (maxHeap.peek() > minHeap.peek()) {
-					minHeap.add(maxHeap.poll());
-					maxHeap.add(minHeap.poll());
-				}
+
+			if (!minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()) {
+				minHeap.add(maxHeap.poll());
+				maxHeap.add(minHeap.poll());
 			}
+			
 			sb.append(maxHeap.peek()).append("\n");
 		}
 		System.out.println(sb);
