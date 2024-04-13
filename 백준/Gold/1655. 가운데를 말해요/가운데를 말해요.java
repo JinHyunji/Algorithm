161d160d9b82201c49acf1648a07023558a568f0
Hyun-ji <131237930+JinHyunji@users.noreply.github.com>
2024-04-13 23:49:13 +0900

    [Gold II] Title: 가운데를 말해요, Time: 472 ms, Memory: 32916 KB -BaekjoonHub

---
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Comparator.reverseOrder());
+		PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+
+		for (int i = 0; i < N; i++) {
+			int a = Integer.parseInt(br.readLine());
+			
+			if (maxHeap.size() == minHeap.size()) {
+				maxHeap.add(a);
+				
+				if (!minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()) {
+					minHeap.add(maxHeap.poll());
+					maxHeap.add(minHeap.poll());
+				}
+			} else {
+				minHeap.add(a);
+				
+				if (maxHeap.peek() > minHeap.peek()) {
+					minHeap.add(maxHeap.poll());
+					maxHeap.add(minHeap.poll());
+				}
+			}
+			sb.append(maxHeap.peek()).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
