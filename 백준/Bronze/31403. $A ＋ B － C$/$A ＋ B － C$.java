e31d46eac8076c8320ae22f39267dcc509e418f6
Henzee <131237930+JinHyunji@users.noreply.github.com>
2024-12-02 13:49:51 +0900

    [Bronze IV] Title: $A + B - C$, Time: 188 ms, Memory: 18412 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        
+        System.out.println(A+B-C);
+        System.out.println(Integer.parseInt("" + A +B) - C);
+    }
+}
