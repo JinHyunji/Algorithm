ca472b77233bad45b3702785f635187ee627e801
Henzee <131237930+JinHyunji@users.noreply.github.com>
2025-01-19 14:48:26 +0900

    [Gold V] Title: 용액, Time: 60 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	vector<int> liquid(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> liquid[i];
+	}
+
+	int start = 0;
+	int end = N - 1;
+	int min = 2000000000;
+	int result[]{ 0,0 };
+
+	while (start < end)
+	{
+		int sum = liquid[start] + liquid[end];
+		int absSum = std::abs(sum);
+
+		if (absSum < min)
+		{
+			result[0] = liquid[start];
+			result[1] = liquid[end];
+			min = absSum;
+		}
+
+		if (sum < 0)
+		{
+			start += 1;
+		}
+		else if (sum > 0)
+		{
+			end -= 1;
+		}
+		else {
+			result[0] = liquid[start];
+			result[1] = liquid[end];
+			break;
+		}
+	}
+
+	if (result[0] < result[1])
+	{
+		cout << result[0] << " " << result[1];
+	}
+	else {
+		cout << result[1] << " " << result[0];
+	}
+}
