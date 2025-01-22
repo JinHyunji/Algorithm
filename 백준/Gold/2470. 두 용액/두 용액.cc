879d0862f7edfce777e10d64327c9b8d50c7fa9e
Henzee <131237930+JinHyunji@users.noreply.github.com>
2025-01-22 14:29:48 +0900

    [Gold V] Title: 두 용액, Time: 16 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.cc
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -1,58 +1,64 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cmath>
-
 using namespace std;
 
-int main() {
-	int N, n;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N;
 	cin >> N;
 
-	vector<int> liquid(N);
-	for (int i = 0; i < N; i++) {
-		cin >> n;
-		liquid[i] = n;
+	vector<int> arr(N);
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
 	}
 
-	sort(liquid.begin(), liquid.end());
+	sort(arr.begin(), arr.end());
+
+	if (N == 2)
+	{
+		cout << arr[0] << " " << arr[1] << endl;
+		return 0;
+	}
 
 	int start = 0;
 	int end = N - 1;
-	int result[]{ 0,0 };
 	int absMin = 2000000000;
+	vector<int> result(2);
 
-	while (start < end) {
-		int sum = liquid[start] + liquid[end];
+	while (start < end)
+	{
+		int sum = arr[start] + arr[end];
 		int absSum = abs(sum);
 
-		if (sum < 0) {
-			if (absSum < absMin) {
-				result[0] = liquid[start];
-				result[1] = liquid[end];
-				absMin = absSum;
-			}
+		if (absSum < absMin)
+		{
+			result[0] = arr[start];
+			result[1] = arr[end];
+			absMin = absSum;
+		}
+
+		if (sum < 0)
+		{
 			start += 1;
 		}
-		else if (sum > 0) {
-			if (absSum < absMin) {
-				result[0] = liquid[start];
-				result[1] = liquid[end];
-				absMin = absSum;
-			}
+		else if (sum > 0)
+		{
 			end -= 1;
 		}
-		else {
-			result[0] = liquid[start];
-			result[1] = liquid[end];
-			break;
+		else
+		{
+			cout << arr[start] << " " << arr[end] << endl;
+			return 0;
 		}
 	}
+	
+	//sort(result.begin(), result.end());
 
-	if (result[0] < result[1]) {
-		cout << result[0] << " " << result[1];
-	}
-	else {
-		cout << result[1] << " " << result[2];
-	}
+	cout << result[0] << " " << result[1];
+	return 0;
 }
