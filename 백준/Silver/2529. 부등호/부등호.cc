6d024c18a8d0b93daba52fa1b9a600584c8e12f7
Henzee <131237930+JinHyunji@users.noreply.github.com>
2025-02-05 12:55:59 +0900

    [Silver I] Title: 부등호, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.cc
+++ 백준/Silver/2529. 부등호/부등호.cc
@@ -2,84 +2,45 @@
 #include <vector>
 #include <algorithm>
 #include <string>
-#include <climits>
+
 using namespace std;
 
 int K;
-long long Max, Min;
 vector<char> signs;
 vector<bool> visited(10);
-vector<int> picked;
+string minNum = "9999999999"; // 충분히 큰 값으로 초기화
+string maxNum = "0000000000"; // 충분히 작은 값으로 초기화
 
-void Pick();
+// 백트래킹 함수
+void backtrack(int depth, string num) {
+    if (depth == K + 1) { // K+1개의 숫자를 선택한 경우
+        if (num < minNum) minNum = num;
+        if (num > maxNum) maxNum = num;
+        return;
+    }
 
-int main()
-{
-	// 입출력 성능 향상
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-
-	// 입력
-	cin >> K;
-
-	signs.resize(K);
-
-	for (int i = 0; i < K; i++)
-	{
-		cin >> signs[i];
-	}
-
-	// 로직
-	Max = 0;
-	Min = LLONG_MAX;
-	
-	// 1) 10개 중에서 K+1개를 고른다.
-	Pick();
-
-	// 출력
-	cout << Max << endl;
-
-	string minStr = to_string(Min);
-	if (minStr.size() < static_cast<size_t>(K + 1))
-		cout << '0';
-	cout << Min << endl;
+    for (int i = 0; i < 10; i++) {
+        if (!visited[i]) {
+            if (depth == 0 || (signs[depth - 1] == '<' && num.back() < '0' + i) || (signs[depth - 1] == '>' && num.back() > '0' + i)) {
+                visited[i] = true;
+                backtrack(depth + 1, num + (char)('0' + i));
+                visited[i] = false;
+            }
+        }
+    }
 }
 
-void Pick()
-{
-	if (picked.size() == static_cast<size_t>(K + 1))
-	{
-		for (size_t i = 0; i < signs.size(); i++)
-		{
-			// 2) 부등호 사이에 들어갈 수 있는지 확인한다.
-			if ((picked[i] > picked[i + 1] && signs[i] == '>') || 
-				(picked[i] < picked[i + 1] && signs[i] == '<'))
-				continue;
-			return;
-		}
-		
-		// 2-1) 최댓값과 최솟값을 갱신한다.
-		string str = "";
-		for (int i : picked)
-			str += to_string(i);
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-		long long num = stoll(str);
-		Max = max(Max, num);
-		Min = min(Min, num);
+    cin >> K;
+    signs.resize(K);
+    for (int i = 0; i < K; i++) cin >> signs[i];
 
-		return;
-	}
+    // 백트래킹 실행
+    backtrack(0, "");
 
-	for (int i = 0; i < 10; i++)
-	{
-		if (!visited[i])
-		{
-			visited[i] = true;
-			picked.push_back(i);
-			Pick();
-			picked.pop_back();
-			visited[i] = false;
-		}
-	}
-
+    // 결과 출력
+    cout << maxNum << "\n" << minNum << "\n";
 }
