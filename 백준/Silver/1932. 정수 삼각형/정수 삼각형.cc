ef993dce56a39df7089be33372f1f35ebc02e9fa
Henzee <131237930+JinHyunji@users.noreply.github.com>
2025-03-03 23:58:09 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 4136 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,15 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <map>
-#include <set>
-#include <string>
-#include <cstdlib>
-#include <cmath>
 #include <algorithm>
 using namespace std;
-using ll = long long;
-
 
 int main()
 {
@@ -22,30 +14,31 @@ int main()
 	vector<vector<int>> triangle(N, vector<int>(N));
 	vector<vector<int>> dp(N, vector<int>(N));
 
+	// 입력 받기
 	for (int i = 0; i < N; i++)
 		for (int j = 0; j <= i; j++)
 			cin >> triangle[i][j];
 
+	// DP 초기값 설정 (꼭대기)
 	dp[0][0] = triangle[0][0];
+
+	// Bottom-Up Dp 계산
 	for (int i = 1; i < N; i++)
 	{
-		dp[i][0] = dp[i - 1][0] + triangle[i][0];
-		dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
-	}
-
-	for (int i = 2; i < N; i++)
-	{
-		for (int j = 1; j < i; j++)
+		for (int j = 0; j <= i; j++)
 		{
-			dp[i][j] = max(dp[i - 1][j - 1] + triangle[i][j], dp[i - 1][j] + triangle[i][j]);		
+			if (j == 0) // 왼쪽 끝
+				dp[i][j] = dp[i - 1][j] + triangle[i][j];
+			else if (j == i) // 오른쪽 끝
+				dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
+			else // 중간 값
+				dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
 		}
 	}
 
-	int result = 0;
-	for (int i = 0; i < N; i++)
-	{
-		result = max(result, dp[N - 1][i]);
-	}
-
+	// 마지막 행에서 최대값 찾기
+	int result = *max_element(dp[N - 1].begin(), dp[N - 1].end());
 	cout << result;
+
+	return 0;
 }
