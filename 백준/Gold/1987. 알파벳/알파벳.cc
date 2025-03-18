2836cdbb858a096887f1f2ad6b79971630569fef
Henzee <131237930+JinHyunji@users.noreply.github.com>
2025-03-18 13:36:35 +0900

    [Gold IV] Title: 알파벳, Time: 336 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,66 +1,50 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <list>
-#include <map>
-#include <set>
 #include <string>
-#include <cstdlib>
-#include <cmath>
-#include <cstdint>
 #include <algorithm>
 using namespace std;
-using ll = long long;
 
-int R, C, maxDist = 1;
+int R, C;
 vector<vector<char>> board;
-bool visited[26];
-int dy[] = { 0, 1, 0, -1 };
-int dx[] = { 1, 0, -1, 0 };
+int dy[] = {0, 1, 0, -1};
+int dx[] = {1, 0, -1, 0};
 
-void DFS(int x, int y, int dist)
-{
-	for (int i = 0; i < 4; i++)
-	{
-		int ny = y + dy[i];
-		int nx = x + dx[i];
+// DFS 함수에서 반환값을 활용하여 최적의 거리 찾기
+int DFS(int x, int y, int visited) {
+    int maxDist = 1;
 
-		if (ny < 0 || ny >= R || nx < 0 || nx >= C) continue;
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
 
-		if (visited[board[ny][nx] - 'A'])
-		{
-			maxDist = max(maxDist, dist);
-			continue;
-		}
+        if (ny < 0 || ny >= R || nx < 0 || nx >= C) continue;
 
-		visited[board[ny][nx] - 'A'] = true;
-		DFS(nx, ny, dist + 1);
-		visited[board[ny][nx] - 'A'] = false;
-	}
+        int nextChar = board[ny][nx] - 'A';
+        if (visited & (1 << nextChar)) continue;  // 이미 방문한 문자면 패스
+
+        // 비트마스크를 이용하여 방문 처리 후 DFS 재귀 호출
+        maxDist = max(maxDist, 1 + DFS(nx, ny, visited | (1 << nextChar)));
+    }
+
+    return maxDist;
 }
 
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr); cout.tie(nullptr);
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
 
-	cin >> R >> C;
+    cin >> R >> C;
+    board.resize(R, vector<char>(C));
 
-	board.resize(R, vector<char>(C));
+    for (int r = 0; r < R; r++) {
+        string input;
+        cin >> input;
+        for (int c = 0; c < C; c++) {
+            board[r][c] = input[c];
+        }
+    }
 
-	for (int r = 0; r < R; r++)
-	{
-		string input;
-		cin >> input;
-
-		for (int c = 0; c < C; c++)
-		{
-			board[r][c] = input[c];
-		}
-	}
-
-	visited[board[0][0] - 'A'] = true;
-	DFS(0, 0, 1);
-	cout << maxDist;
-	return 0;
+    int startChar = board[0][0] - 'A';
+    cout << DFS(0, 0, 1 << startChar);  // 초기 방문 상태: 1 << (첫 문자 위치)
+    return 0;
 }
