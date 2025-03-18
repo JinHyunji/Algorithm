ae70fe4e49695c8ced54ca86394a5d02148bc435
Henzee <131237930+JinHyunji@users.noreply.github.com>
2025-03-18 12:49:30 +0900

    [Gold IV] Title: 알파벳, Time: 400 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <list>
+#include <map>
+#include <set>
 #include <string>
+#include <cstdlib>
+#include <cmath>
+#include <cstdint>
 #include <algorithm>
 using namespace std;
+using ll = long long;
 
 int R, C, maxDist = 1;
-vector<vector<int>> board;
+vector<vector<char>> board;
 bool visited[26];
 int dy[] = { 0, 1, 0, -1 };
 int dx[] = { 1, 0, -1, 0 };
@@ -19,15 +27,15 @@ void DFS(int x, int y, int dist)
 
 		if (ny < 0 || ny >= R || nx < 0 || nx >= C) continue;
 
-		if (visited[board[ny][nx] - 65])
+		if (visited[board[ny][nx] - 'A'])
 		{
 			maxDist = max(maxDist, dist);
 			continue;
 		}
 
-		visited[board[ny][nx] - 65] = true;
+		visited[board[ny][nx] - 'A'] = true;
 		DFS(nx, ny, dist + 1);
-		visited[board[ny][nx] - 65] = false;
+		visited[board[ny][nx] - 'A'] = false;
 	}
 }
 
@@ -38,7 +46,7 @@ int main()
 
 	cin >> R >> C;
 
-	board.resize(R, vector<int>(C, 0));
+	board.resize(R, vector<char>(C));
 
 	for (int r = 0; r < R; r++)
 	{
@@ -51,7 +59,7 @@ int main()
 		}
 	}
 
-	visited[board[0][0] - 65] = true;
+	visited[board[0][0] - 'A'] = true;
 	DFS(0, 0, 1);
 	cout << maxDist;
 	return 0;
