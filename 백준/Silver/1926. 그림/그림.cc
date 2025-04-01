2757879a57ea2f93c620b85f0338e28e8ced7b2b
Henzee <131237930+JinHyunji@users.noreply.github.com>
2025-04-01 12:00:00 +0900

    [Silver I] Title: 그림, Time: 20 ms, Memory: 10924 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -6,10 +6,12 @@ int map[500][500];
 bool visited[500][500];
 int dy[] = { 0, 1, 0, -1 };
 int dx[] = { 1, 0, -1, 0 };
+int area; // 현재 그림의 크기
 
-void DFS(int x, int y, int& w)
+void DFS(int x, int y)
 {
 	visited[y][x] = true;
+	area++; // 현재 그림의 크기 증가
 
 	for (int i = 0; i < 4; i++)
 	{
@@ -19,8 +21,7 @@ void DFS(int x, int y, int& w)
 		if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
 		if (visited[ny][nx] || map[ny][nx] == 0) continue;
 
-		w++;
-		DFS(nx, ny, w);
+		DFS(nx, ny);
 	}
 }
 
@@ -41,10 +42,10 @@ int main()
 		{
 			if (!visited[y][x] && map[y][x] == 1)
 			{
-				int widthCnt = 1;
-				DFS(x, y, widthCnt);
-				if (widthCnt > maxW) maxW = widthCnt;
+				area = 0; // 새 그림 시작 시 크기 초기화
+				DFS(x, y);
 				cnt++;
+				maxW = max(maxW, area); // 가장 큰 그림 크기 업데이트
 			}
 		}
 	}
