a23b012ada34b847c642850fe4b431b2e921b8bb
진현지 <jinhyunji520@gmail.com>
2025-05-02 11:21:46 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 8 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
@@ -6,21 +6,21 @@ using namespace std;
 #define MAX 500000
 
 int N, K;
-bool visited[2][MAX + 1] = { false }; // 짝수, 홀수 시간 배열로 방문 관리
+bool visited[2][MAX + 1] = { false };
 
 int main()
 {
 	cin >> N >> K;
 
 	queue<int> q;
-	q.push(N); // 수빈이의 위치
+	q.emplace(N);
 	visited[0][N] = true;
 
 	int t = 0;
 	while (!q.empty())
 	{
-		int sister = K + t * (t + 1) / 2; // t초 후 동생의 위치
-
+		int sister = K + t * (t + 1) / 2;
+		
 		if (sister > MAX)
 		{
 			cout << -1;
@@ -33,21 +33,23 @@ int main()
 			return 0;
 		}
 
-		int size = q.size();
-		for (int i = 0; i < size; ++i)
+		int qSize = q.size();
+		for (int i = 0; i < qSize; i++)
 		{
 			int cur = q.front(); q.pop();
 
-			for (int next : { cur - 1, cur + 1, cur * 2 })
+			for (int next : {cur + 1, cur - 1, cur * 2})
 			{
 				if (next < 0 || next > MAX) continue;
 				if (visited[(t + 1) % 2][next]) continue;
 
 				visited[(t + 1) % 2][next] = true;
-				q.push(next);
+				q.emplace(next);
 			}
 		}
 
 		t++;
 	}
+
+	return 0;
 }
