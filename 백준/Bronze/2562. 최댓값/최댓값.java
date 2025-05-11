66e85836245538d583d4e33abdccbb349e11113c
진현지 <jinhyunji520@gmail.com>
2025-05-11 21:06:02 +0900

    [Bronze III] Title: 최댓값, Time: 208 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	int[] arr = new int[10];
+    	int index = 0;
+    	int max = 0;
+    	for(int i=1; i < 10; i++) {
+    		arr[i] = sc.nextInt();
+    		if(arr[i] > max) {
+    			max = arr[i];
+    			index = i;
+    		}
+    	}
+    	System.out.println(max);
+    	System.out.println(index);
+    }
+}
