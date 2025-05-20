ba3f44bc7fe32f1f8b0cf60e845e06277b4a48f6
진현지 <jinhyunji520@gmail.com>
2025-05-20 11:49:24 +0900

    [Silver III] Title: 회전하는 큐, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
@@ -1,46 +1,6 @@
 #include <iostream>
-#include <string>
 #include <deque>
 using namespace std;
-
-const int MX = 50;
-int dat[2 * MX + 1];
-int head = MX, tail = MX;
-
-void push_front(int x) {
-	dat[--head] = x;
-}
-
-void push_back(int x) {
-	dat[tail++] = x;
-}
-
-int size()
-{
-	return tail - head;
-}
-
-bool empty()
-{
-	return size() == 0 ? true : false;
-}
-
-void pop_front() {
-	head++;
-}
-
-void pop_back() {
-	tail--;
-}
-
-int front() {
-	return dat[head];
-}
-
-int back() {
-	return dat[tail - 1];
-}
-
 
 int main(void) {
 
