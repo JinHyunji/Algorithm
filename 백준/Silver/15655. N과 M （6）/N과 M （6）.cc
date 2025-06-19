c020ae815858dc3475b17c26ea6eaa80c2a9f783
진현지 <jinhyunji520@gmail.com>
2025-06-19 13:00:07 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -3,42 +3,27 @@
 #include <algorithm>
 using namespace std;
 
-int N, M;
-vector<int> arr;
-vector<int> picked;
-vector<vector<int>> answer;
-
-void func(int idx, int sIdx)
-{
-	if (sIdx == M)
-	{
-		answer.emplace_back(picked);
-		return;
-	}
-
-	if (idx == N) return;
-
-	picked[sIdx] = arr[idx];
-	func(idx + 1, sIdx + 1);
-	func(idx + 1, sIdx);
-}
-
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr); cout.tie(nullptr);
 
+	int N, M;
 	cin >> N >> M;
-	arr.resize(N);
-	picked.resize(M, 0);
+	vector<int> arr(N);
 	for (int i = 0; i < N; i++)
 		cin >> arr[i];
 	sort(arr.begin(), arr.end());
-	func(0, 0);
-	for (const auto& ans : answer)
+
+	vector<int> selected;
+	for (int i = 0; i < N; i++)
+		selected.emplace_back(i < M ? 0 : 1);
+
+	do
 	{
-		for (int i = 0; i < M; i++)
-			cout << ans[i] << ' ';
+		for (int i = 0; i < N; i++)
+			if (selected[i] == 0)
+				cout << arr[i] << ' ';
 		cout << '\n';
-	}
+	} while (next_permutation(selected.begin(), selected.end()));
 }
