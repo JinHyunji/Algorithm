03e3ab67ce72f67a2109d8bf8a18d073d9f66c61
진현지 <jinhyunji520@gmail.com>
2025-06-24 11:25:04 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int arr[8];
+bool isUsed[8];
+int picked[8];
+
+void func(int k, int preIdx)
+{
+	if (k == M)
+	{
+		for (int i = 0; i < M; i++)
+			cout << picked[i] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	int temp = 0; // 이전에 넣었던 수
+	for (int i = preIdx; i < N; i++)
+	{
+		if (!isUsed[i] && temp != arr[i])
+		{
+			isUsed[i] = true;
+			temp = arr[i];
+			picked[k] = arr[i];
+			func(k + 1, i);
+			isUsed[i] = false;
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+	sort(arr, arr + N);
+	func(0, 0);
+}
