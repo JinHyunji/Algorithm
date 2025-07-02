9c4a730a8910ca6e0ae6f4961cebcaa4c9106de8
진현지 <jinhyunji520@gmail.com>
2025-07-02 15:19:58 +0900

    [Silver III] Title: N과 M (3), Time: 340 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -2,27 +2,33 @@
 using namespace std;
 
 int N, M;
-int arr[7];
+int picked[10];
 
-void func(int k)
+void func(int k) 
 {
 	if (k == M)
 	{
 		for (int i = 0; i < M; i++)
-			cout << arr[i] << ' ';
+		{
+			cout << picked[i] << ' ';
+		}
 		cout << '\n';
 		return;
 	}
 
 	for (int i = 1; i <= N; i++)
 	{
-		arr[k] = i;
+		picked[k] = i;
 		func(k + 1);
 	}
 }
 
 int main()
 {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
 	cin >> N >> M;
 	func(0);
+
 }
