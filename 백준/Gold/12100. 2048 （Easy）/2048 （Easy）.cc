896df5195855c7d9d146c1f468d3399c6f522961
진현지 <jinhyunji520@gmail.com>
2025-07-08 10:48:27 +0900

    [Gold I] Title: 2048 (Easy), Time: 8 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+#define X first
+#define Y second
+
+int N;
+int board[20][20];
+int board2[20][20];
+
+void rotate()
+{
+	int temp[20][20];
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			temp[j][N - 1 - i] = board2[i][j];
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			board2[i][j] = temp[i][j];
+}
+
+void tilt(int dir)
+{
+	while (dir--) rotate();
+
+	for (int i = 0; i < N; i++)
+	{
+		int tilted[20] = {};
+		int idx = 0;
+		for (int j = 0; j < N; j++)
+		{
+			if (board2[i][j] == 0) continue;
+			if (tilted[idx] == 0)
+				tilted [idx] = board2[i][j];
+			else if (tilted[idx] == board2[i][j])
+				tilted[idx++] *= 2;
+			else
+				tilted[++idx] = board2[i][j];
+		}
+		for (int j = 0; j < N; j++)
+			board2[i][j] = tilted[j];
+	}
+
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> N;
+
+	for (int x = 0; x < N; x++)
+		for (int y = 0; y < N; y++)
+			cin >> board[x][y];
+
+	int mx = 0;
+	for (int i = 0; i < 1024; i++)
+	{
+		for (int x = 0; x < N; x++)
+			for (int y = 0; y < N; y++)
+				board2[x][y] = board[x][y];
+
+		int brute = i;
+		for (int j = 0; j < 5; j++)
+		{
+			int dir = brute % 4;
+			brute /= 4;
+			tilt(dir);
+		}
+
+		for (int x = 0; x < N; x++)
+			for (int y = 0; y < N; y++)
+				mx = max(mx, board2[x][y]);
+	}
+
+	cout << mx;
+}
