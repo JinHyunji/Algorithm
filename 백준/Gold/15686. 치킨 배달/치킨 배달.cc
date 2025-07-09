ed8b349ed6642e904e541de4a7a444e5666bda41
진현지 <jinhyunji520@gmail.com>
2025-07-09 13:02:04 +0900

    [Gold V] Title: 치킨 배달, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,84 +1,72 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <climits>
+#include <vector>
 using namespace std;
 
-int N, M, answer;
-vector<vector<int>> map; // 전체 지도 저장
-vector<pair<int, int>> chickens; // 치킨집 위치 저장
-vector<pair<int, int>> home; // 집 위치 저장
-vector<pair<int, int>> picked; // 고른 치킨집 위치 저장
+#define X first
+#define Y second
 
-void Pick(int idx, int sIdx);
-void Chicken(vector<pair<int, int>>& chicken);
-int Dist(pair<int, int>& pos1, pair<int, int>& pos2);
+int N, M, chickenCnt = 0, houseCnt = 0, answer = INT32_MAX;
+int map[50][50];
+vector<pair<int, int>> picked(13);
+vector<pair<int, int>> houses;
+vector<pair<int, int>> chickens;
 
-int main()
+int distance(pair<int, int> R, pair<int, int> C)
 {
-	// 입출력 성능 향상
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-
-	// 입력
-	cin >> N >> M;
-	answer = INT_MAX;
-	map.resize(N, vector<int>(N));
-
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			cin >> map[i][j];
-			
-			if (map[i][j] == 2) chickens.push_back({ i, j }); // 치킨집 위치 저장
-			else if (map[i][j] == 1) home.push_back({ i, j }); // 집 위치 저장
-		}
-	}
-
-	picked.resize(M);
-
-	// M개의 치킨집 고르기
-	Pick(0, 0);
-
-	// 출력
-	cout << answer;
+	return abs(R.X - C.X) + abs(R.Y - C.Y);
 }
 
-void Pick(int idx, int sIdx)
+void func(int idx, int sIdx)
 {
-	// 다 골랐으면
 	if (sIdx == M)
 	{
-		// 도시의 치킨 거리 구하기
-		Chicken(picked);
+		int sum = 0;
+		for (int i = 0; i < houseCnt; i++)
+		{
+			int mnDist = INT32_MAX;
+			for (int j = 0; j < M; j++)
+			{
+				mnDist = min(mnDist, distance(houses[i], picked[j]));
+			}
+			sum += mnDist;
+		}
+		answer = min(answer, sum);
 		return;
 	}
 
-	if (idx == chickens.size()) return;
+	if (idx == chickenCnt) return;
 
-	picked[sIdx] = chickens[idx]; // 고른 치킨집 담기
-	Pick(idx + 1, sIdx + 1); // 다음 요소 담기
-	Pick(idx + 1, sIdx); // 다음 요소 안 담기
+	picked[sIdx] = chickens[idx];
+	func(idx + 1, sIdx + 1);
+	func(idx + 1, sIdx);
 }
 
-void Chicken(vector<pair<int, int>>& chicken)
+int main()
 {
-	int sum = 0;
-	for (int i = 0; i < home.size(); i++)
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> N >> M;
+
+	for (int x = 0; x < N; x++)
 	{
-		int minChicken = INT_MAX;
-		for (int j = 0; j < chicken.size(); j++)
+		for (int y = 0; y < N; y++)
 		{
-			minChicken = min(minChicken, Dist(home[i], chicken[j])); // 치킨 거리의 최소값 구하기
+			cin >> map[x][y];
+			if (map[x][y] == 1)
+			{
+				houses.emplace_back(x, y);
+				houseCnt++;
+			}
+			if (map[x][y] == 2)
+			{
+				chickens.emplace_back(x, y);
+				chickenCnt++;
+			}
 		}
-		sum += minChicken; // 도시의 치킨 거리 구하기
 	}
 
-	answer = min(sum, answer); // 도시의 치킨 거리의 최소값 구하기
-}
-
-int Dist(pair<int, int>& pos1, pair<int, int>& pos2)
-{
-	return abs(pos1.first - pos2.first) + abs(pos1.second - pos2.second);
+	func(0, 0);
+	cout << answer;
 }
