933c4c6214577a85079012d034623a8a10c39920
진현지 <jinhyunji520@gmail.com>
2025-07-21 12:51:54 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.cc
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.cc
@@ -8,137 +8,73 @@ using namespace std;
 int N, M, X, Y, K;
 int map[30][30];
 int dice[7]; // 위, 뒤, 동, 서, 앞, 아래
+int dx[] = { 0, 0, -1, 1 }; // 동, 서, 북, 남
+int dy[] = { 1, -1, 0, 0 };
 
-void Dice(int dir)
+void Rotate(int dir)
 {
-	if (dir == 1) // 동
+	int temp[7];
+	switch (dir)
 	{
-		int nx = X;
-		int ny = Y + 1;
-
-		if (nx < 0 || ny < 0 || nx >= N || ny >= M) return; // 지도 바깥이면 명령 무시
-
-		// 주사위 이동
-		int temp[7];
+	case 1:
 		temp[1] = dice[4];
 		temp[2] = dice[2];
 		temp[3] = dice[1];
 		temp[4] = dice[6];
 		temp[5] = dice[5];
 		temp[6] = dice[3];
-
-		copy(begin(temp), end(temp), dice);
-
-		if (map[nx][ny] == 0)
-		{
-			map[nx][ny] = dice[6];
-		}
-		else
-		{
-			dice[6] = map[nx][ny];
-			map[nx][ny] = 0;
-		}
-
-		cout << dice[1] << '\n';
-		X = nx, Y = ny;
-		return;
-	}
-	else if (dir == 2) // 서
-	{
-		int nx = X;
-		int ny = Y - 1;
-
-		if (nx < 0 || ny < 0 || nx >= N || ny >= M) return; // 지도 바깥이면 명령 무시
-
-		// 주사위 이동
-		int temp[7];
+		break;
+	case 2:
 		temp[1] = dice[3];
 		temp[2] = dice[2];
 		temp[3] = dice[6];
 		temp[4] = dice[1];
 		temp[5] = dice[5];
 		temp[6] = dice[4];
-
-		copy(begin(temp), end(temp), dice);
-
-		if (map[nx][ny] == 0)
-		{
-			map[nx][ny] = dice[6];
-		}
-		else
-		{
-			dice[6] = map[nx][ny];
-			map[nx][ny] = 0;
-		}
-
-		cout << dice[1] << '\n';
-		X = nx, Y = ny;
-		return;
-	}
-	else if (dir == 3) // 북
-	{
-		int nx = X - 1;
-		int ny = Y;
-
-		if (nx < 0 || ny < 0 || nx >= N || ny >= M) return; // 지도 바깥이면 명령 무시
-
-		// 주사위 이동
-		int temp[7];
+		break;
+	case 3:
 		temp[1] = dice[5];
 		temp[2] = dice[1];
 		temp[3] = dice[3];
 		temp[4] = dice[4];
 		temp[5] = dice[6];
 		temp[6] = dice[2];
-
-		copy(begin(temp), end(temp), dice);
-
-		if (map[nx][ny] == 0)
-		{
-			map[nx][ny] = dice[6];
-		}
-		else
-		{
-			dice[6] = map[nx][ny];
-			map[nx][ny] = 0;
-		}
-
-		cout << dice[1] << '\n';
-		X = nx, Y = ny;
-		return;
-	}
-	else // 남
-	{
-		int nx = X + 1;
-		int ny = Y;
-
-		if (nx < 0 || ny < 0 || nx >= N || ny >= M) return; // 지도 바깥이면 명령 무시
-
-		// 주사위 이동
-		int temp[7];
+		break;
+	case 4:
 		temp[1] = dice[2];
 		temp[2] = dice[6];
 		temp[3] = dice[3];
 		temp[4] = dice[4];
 		temp[5] = dice[1];
 		temp[6] = dice[5];
-
-		copy(begin(temp), end(temp), dice);
-
-		if (map[nx][ny] == 0)
-		{
-			map[nx][ny] = dice[6];
-		}
-		else
-		{
-			dice[6] = map[nx][ny];
-			map[nx][ny] = 0;
-		}
-
-		cout << dice[1] << '\n';
-		X = nx, Y = ny;
-		return;
+		break;
 	}
+	copy(begin(temp), end(temp), dice);
+}
+
+void Dice(int dir)
+{
+	int nx = X, ny = Y;
+	nx += dx[dir - 1];
+	ny += dy[dir - 1];
+
+	if (nx < 0 || ny < 0 || nx >= N || ny >= M) return; // 지도 바깥이면 명령 무시
+
+	// 주사위 이동
+	Rotate(dir);
+
+	if (map[nx][ny] == 0)
+	{
+		map[nx][ny] = dice[6];
+	}
+	else
+	{
+		dice[6] = map[nx][ny];
+		map[nx][ny] = 0;
+	}
+
+	cout << dice[1] << '\n';
+	X = nx, Y = ny;
 }
 
 int main()
